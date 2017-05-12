5500db520bdce8e37038745b9e240fcb32ccfb0e
Ching. Mali (GSM) <Mali_Ching@compalcomm.com>
2017-05-12 14:05:53 +0800

    ex6 mission accomplished

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -8,6 +8,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
+
 C = 1;
 sigma = 0.3;
 
@@ -23,12 +24,33 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C1 = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigma1 = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+Param = zeros(numel(C1)*numel(sigma1), 2); % [64*2]
 
+for i=1:numel(C1)
+    for j=1:numel(sigma1)
+        Param(j+(i-1)*numel(C1),1:2)=[C1(i) sigma1(j)];
+    end
+end
 
-
-
-
-
+Counts = zeros(2,1);
+tempCounts = 0;
+idx = 0;
+for i=1:size(Param, 1)
+    model= svmTrain(X, y, Param(i,1), @(x1, x2) gaussianKernel(x1, x2, Param(i,2))); 
+    predictions = svmPredict(model, Xval);
+    if mean(double(predictions)) == mean(double(yval))     
+        temp = predictions == yval;
+        Counts = arrayfun( @(x)sum(temp==1), unique(temp));
+        if Counts(2) > tempCounts
+            tempCounts = Counts;
+            idx = i;
+        end
+    end
+end
+C = Param(idx, 1);
+sigma = Param(idx, 2);
 % =========================================================================
 
 end
